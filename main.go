a9ed919619db179d31c8cce604808db01c90fd51
b.ottenwaelder@gmail.com <b.ottenwaelder@gmail.com>
2017-01-08 22:35:36 +0100

    Introduced a map to hold all systems

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-//Copyright 2016 Beate Ottenwälder
+//Copyright 2016-2017 Beate Ottenwälder
 //
 //Licensed under the Apache License, Version 2.0 (the "License");
 //you may not use this file except in compliance with the License.
@@ -21,6 +21,8 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"io"
+	"crypto/rand"
 )
 
 //todo: refactor and move to own file
@@ -43,7 +45,7 @@ type repository interface {
 	putFile(file File) error
 }
 
-//var s = make([]System, 1)
+var s = make(map[string]System, 1)
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
@@ -51,12 +53,14 @@ func main() {
 	router.HandleFunc("/files/{file}", getFile).Methods("GET")
 	router.HandleFunc("/files/{file}", putFile).Methods("PUT")
 	router.HandleFunc("/systems/{system}", getSystem).Methods("GET")
-	router.HandleFunc("/systems/{system}", putSystem).Methods("PUT")
+	router.HandleFunc("/systems", postSystem).Methods("POST")
+	//router.HandleFunc("/systems/{system}", postSystem).Methods("PUT")
+	router.HandleFunc("/systems/{system}", delSystem).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func home(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Remote File Lookup")
+	fmt.Fprintf(writer, "Remote File Lookup is up")
 }
 
 //todo: return list of files that match the search pattern
@@ -76,10 +80,31 @@ func getSystem(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintln(writer, "List of files that are found for:", params["system"])
 }
 
-func putSystem(writer http.ResponseWriter, request *http.Request) {
+func delSystem(writer http.ResponseWriter, request *http.Request) {
+	params := mux.Vars(request)
+	delete(s, params["system"])
+	fmt.Fprintln(writer, "Deleting system:", params["system"])
+}
+
+//Source: https://play.golang.org/p/4FkNSiUDMg
+func newUUID() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+	// variant bits; see section 4.1.1
+	uuid[8] = uuid[8] &^ 0xc0 | 0x80
+	// version 4 (pseudo-random); see section 4.1.3
+	uuid[6] = uuid[6] &^ 0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
+}
+
+func postSystem(writer http.ResponseWriter, request *http.Request) {
 	var system System
 	err := json.NewDecoder(request.Body).Decode(&system)
-	//append(s, system)
+	newSystem, _ := newUUID()
+	s[newSystem] = system
 	if err != nil {
 		fmt.Fprintln(writer, "Error during conversion")
 	} else {
